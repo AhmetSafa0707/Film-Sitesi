69fe92fb16a52cdde4760237f2d3d76883868004
AhmetSafa0707 <119118562+AhmetSafa0707@users.noreply.github.com>
2023-06-05 12:33:02 +0300

    Responsive Tasarım ve Dark-Light Mode
    -Ekranın büyüklüğüne göre değişen tasarım uyguladım.
    -Light Mode için tasarım yaptım ve JS kısmını yazdım.

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const movieLists = document.querySelectorAll(".movie-list")
 
 
 arrows.forEach((arrow, i) => {
+    const widthRatio=Math.floor(window.innerWidth/300);
     let clickCounter = 0;
     const imageItem = movieLists[i].querySelectorAll("img").length;
     arrow.addEventListener("click", function () {
         clickCounter++;
-        if (imageItem - (4 + clickCounter) >= 0) {
+        if (imageItem - (4 + clickCounter)+(4-widthRatio) >= 0) {
             movieLists[i].style.transform = `translateX(${movieLists[i].
                 computedStyleMap().get("transform")[0].x.value - 300
-            }px)`;
+                }px)`;
         } else {
             movieLists[i].style.transform = "translateX(0)";
+            clickCounter=0;
         }
     });
 
-});  
+});
+
+/*  DARK MODE  */
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.navbar,.logo,.menu-list-item,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-item-title,.movie-list-title")
+ball.addEventListener("click", function () {
+items.forEach((item)=>item.classList.toggle("active"));
+})
